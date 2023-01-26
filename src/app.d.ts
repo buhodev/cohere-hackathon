45a144f112a1f458cec5cfc0555c76327fe7a051
Carlos Ivanchuk <buhodev@proton.me>
2023-01-26 11:58:20 -0400

    fix: make module declarations global

--- src/app.d.ts
+++ src/app.d.ts
@@ -14,30 +14,29 @@ declare global {
 		// interface PageData {}
 		// interface Platform {}
 	}
-}
+	module '*.svg?component' {
+		import type { ComponentType, SvelteComponentTyped } from 'svelte';
+		import type { SVGAttributes } from 'svelte/elements';
 
-declare module '*.svg?component' {
-	import type { ComponentType, SvelteComponentTyped } from 'svelte';
-	import type { SVGAttributes } from 'svelte/elements';
+		const content: ComponentType<SvelteComponentTyped<SVGAttributes<SVGSVGElement>>>;
 
-	const content: ComponentType<SvelteComponentTyped<SVGAttributes<SVGSVGElement>>>;
+		export default content;
+	}
 
-	export default content;
-}
+	module '*.svg?src' {
+		const content: string;
+		export default content;
+	}
 
-declare module '*.svg?src' {
-	const content: string;
-	export default content;
-}
+	module '*.svg?url' {
+		const content: string;
+		export default content;
+	}
 
-declare module '*.svg?url' {
-	const content: string;
-	export default content;
-}
-
-declare module '*.svg?dataurl' {
-	const content: string;
-	export default content;
+	module '*.svg?dataurl' {
+		const content: string;
+		export default content;
+	}
 }
 
 export {};
