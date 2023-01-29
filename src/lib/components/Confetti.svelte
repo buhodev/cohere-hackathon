27c49ad7d0f895529897f8e19b5c16e144833b48
Carlos Ivanchuk <buhodev@proton.me>
2023-01-28 21:02:53 -0400

    refactor: fire the confetti lower in the page

--- src/lib/components/Confetti.svelte
+++ src/lib/components/Confetti.svelte
@@ -4,7 +4,7 @@
 </script>
 
 {#if $showConfetti}
-	<div class="pointer-events-none flex justify-center">
+	<div class="pointer-events-none absolute inset-0 top-32 flex justify-center">
 		<div use:confetti />
 	</div>
 {/if}
