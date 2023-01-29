faee6233ef0f4c7bbcb52a3dd261df17f60cd1f0
Carlos Ivanchuk <buhodev@proton.me>
2023-01-28 21:20:41 -0400

    refactor: move utilities to their own file

--- src/lib/components/HeadHrefLangs.svelte
+++ src/lib/components/HeadHrefLangs.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { locales, baseLocale } from '$i18n/i18n-util';
 	import { page } from '$app/stores';
-	import { replaceLocaleInUrl } from '$lib/utils';
+	import { replaceLocaleInUrl } from '$lib/utils/replaceLocaleInUrl';
 </script>
 
 {#each locales as l}
