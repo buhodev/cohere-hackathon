548333d8698199de16b8bb3fe893cc5f6ee202a8
Carlos Ivanchuk <buhodev@proton.me>
2023-02-02 16:16:04 -0400

    feat: add RangeSlider component

---
+++ src/lib/components/RangeSlider.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let min = 0;
+	export let max = 100;
+	export let label: string;
+	export let name: string;
+	export let value: number;
+	export let step: number;
+</script>
+
+<label for={name}>{label}</label>
+<input type="range" id={name} {name} {value} {min} {max} {step} />
