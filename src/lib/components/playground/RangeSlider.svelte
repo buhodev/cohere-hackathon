f54aafba5b9f0c9d1c8e4f5895733c700709d99b
Carlos Ivanchuk <buhodev@proton.me>
2023-02-03 12:22:33 -0400

    refactor: improve RangeSlider design

---
+++ src/lib/components/playground/RangeSlider.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import InputHeading from '$lib/components/playground/InputHeading.svelte';
+
+	export let label: string;
+	export let subtitle: string | undefined = undefined;
+	export let name: string;
+	export let value: number;
+	export let min: number;
+	export let max: number;
+	export let step: number;
+</script>
+
+<div>
+	<InputHeading {name} {value} {label} {subtitle} />
+	<input type="range" bind:value id={name} {name} {min} {max} {step} />
+</div>
