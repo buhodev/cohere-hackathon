ea49c69a3fc432a2fc58a77c6eeb08db22866db1
Carlos Ivanchuk <buhodev@proton.me>
2023-02-03 12:25:55 -0400

    feat: add InputSelect component

---
+++ src/lib/components/playground/InputSelect.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import InputHeading from '$lib/components/playground/InputHeading.svelte';
+
+	export let label: string;
+	export let name: string;
+	export let value: 'command-xlarge-nightly' | 'xlarge' | 'medium';
+</script>
+
+<div>
+	<InputHeading {label} {name} />
+	<select
+		{name}
+		id={name}
+		bind:value
+		class="mt-1 block w-full rounded-md border-neutral-300 bg-white py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-neutral-700 dark:bg-neutral-900 sm:text-sm"
+	>
+		<option>command-xlarge-nightly</option>
+		<option>xlarge</option>
+		<option>medium</option>
+	</select>
+</div>
