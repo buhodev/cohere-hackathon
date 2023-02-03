a26b59c45244fbefed9780123072e69bc1239f2b
Carlos Ivanchuk <buhodev@proton.me>
2023-02-03 17:51:39 -0400

    refactor: extract switchLocale function

--- src/lib/components/LocaleSwitcher.svelte
+++ src/lib/components/LocaleSwitcher.svelte
@@ -1,31 +1,14 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import { invalidateAll } from '$app/navigation';
 	import { page } from '$app/stores';
-	import { setLocale, locale } from '$i18n/i18n-svelte';
+	import { locale } from '$i18n/i18n-svelte';
 	import type { Locales } from '$i18n/i18n-types';
 	import { locales } from '$i18n/i18n-util';
-	import { loadLocaleAsync } from '$i18n/i18n-util.async';
-	import { replaceLocaleInUrl } from '$lib/utils';
+	import { replaceLocaleInUrl, switchLocale } from '$lib/utils';
 	import { LL } from '$i18n/i18n-svelte';
 
 	let value: Locales = $page.params.lang as Locales;
 
-	const switchLocale = async (newLocale: Locales, updateHistoryState = true) => {
-		if (!newLocale || $locale === newLocale) return;
-		// load new dictionary from server
-		await loadLocaleAsync(newLocale);
-		// select locale
-		setLocale(newLocale);
-		// update `lang` attribute
-		document.querySelector('html')?.setAttribute('lang', newLocale);
-		if (updateHistoryState) {
-			// update url to reflect locale changes
-			history.pushState({ locale: newLocale }, '', replaceLocaleInUrl($page.url, newLocale));
-		}
-		// run the `load` function again
-		invalidateAll();
-	};
 	// update locale when navigating via browser back/forward buttons
 	const handlePopStateEvent = async ({ state }: PopStateEvent) => switchLocale(state.locale, false);
 	// update locale when page store changes
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,5 +2,6 @@ import { getMessagePosition } from '$lib/utils/getMessagePosition';
 import { getOS } from '$lib/utils/getOS';
 import { replaceLocaleInUrl } from '$lib/utils/replaceLocaleInUrl';
 import { getCommand } from '$lib/utils/getCommand';
+import { switchLocale } from '$lib/utils/switchLocale';
 
-export { getMessagePosition, getOS, replaceLocaleInUrl, getCommand };
+export { getMessagePosition, getOS, replaceLocaleInUrl, getCommand, switchLocale };
