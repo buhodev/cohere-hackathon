daa221e9ec02a4b094ea41bad3ab9f9c0ecfeec9
Carlos Ivanchuk <buhodev@proton.me>
2023-02-04 01:17:06 -0400

    refactor: extract toggleTheme function

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -3,5 +3,6 @@ import { getOS } from '$lib/utils/getOS';
 import { replaceLocaleInUrl } from '$lib/utils/replaceLocaleInUrl';
 import { getCommand } from '$lib/utils/getCommand';
 import { switchLocale } from '$lib/utils/switchLocale';
+import { toggleTheme } from '$lib/utils/toggleTheme';
 
-export { getMessagePosition, getOS, replaceLocaleInUrl, getCommand, switchLocale };
+export { getMessagePosition, getOS, replaceLocaleInUrl, getCommand, switchLocale, toggleTheme };
